9c365c430dd66e0abfa2aacf1369ba7dfac3757c
Farhath <abdulfarhathmohd@gmail.com>
2025-02-08 00:24:29 +0530

    maze

--- Recursion/maze.java
+++ Recursion/maze.java
@@ -1,1 +1,26 @@
-maze.java
+import java.util.*;
+public class maze{
+	public static void main(String[] args) {
+		System.out.println(recursion("", 3,3));
+	}
+	static ArrayList<String> recursion(String sb,int r , int c){
+		if(r == 1 && c == 1){
+			ArrayList<String> list = new ArrayList<>();
+			list.add(sb);
+			return list;
+		}
+		ArrayList<String> left = new ArrayList<>(),right = new ArrayList<>(),diag = new ArrayList<>();
+		if(r>1 && c>1){
+			diag = recursion(sb+"D",r-1,c-1);
+		}
+		if(r > 1){
+			left = recursion(sb+"r",r-1,c);
+		}
+		if(c > 1){
+			right = recursion(sb+"d",r,c-1);
+		}
+		left.addAll(right);
+		left.addAll(diag);
+		return left;
+	}
+}
---
+++ Recursion/maze_obstacles_lc1926.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int nearestExit(char[][] maze, int[] entrance) {
+        
+    }
+
+    static int backtracking(char[][] maze, int[] entrance,int rlen, int clen, int r, int c){
+
+        if(maze[r][c] == '+'){
+            return 0;
+        }else{
+            return 1; //dbt
+        }
+
+        //up
+        if(r>0){
+            
+        }
+        //down
+        if(r<rlen){
+
+        }
+        //left
+        if(c>0){
+
+        }
+        //right
+        if(c<clen){
+
+        }
+    }
+}
