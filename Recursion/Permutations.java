c6cb671e0bb761e088c5e3295b9bddaff9aae9d1
Farhath <abdulfarhathmohd@gmail.com>
2025-02-11 03:57:40 +0530

    minor changes

--- Recursion/Permutations.java
+++ Recursion/Permutations.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Permutations {
     
     static void printPermutations(int[] arr, int idx){
@@ -19,7 +20,11 @@ public class Permutations {
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,2,3};
+        // int[] arr = {1,2,3};
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[3];
+        for(int i = 0; i<3; i++) arr[i] = sc.nextInt();
+        sc.close();
         printPermutations(arr, 0);
     }
 }
--- Recursion/wordsearch_79.java
+++ Recursion/wordsearch_79.java
@@ -2,39 +2,33 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         int r = board.length;
         int c = board[0].length;
+        if(r == 1 && c == 1) return (Character.toString(board[0][0]).equals(word)); 
         boolean visited[][] = new boolean[r][c];
         for(int i = 0; i<r; i++){
             for(int j = 0; j<c; j++){
-                if(recursion(board, word, "", visited, i, j)) return true;        
+                if(recursion(board, word, 0, visited, i, j)) return true;        
             }
         }
         return false;
     }
-    static boolean recursion(char[][] board, String word, String sb, boolean[][] visited, int r, int c){
+    static boolean recursion(char[][] board, String word, int idx, boolean[][] visited, int r, int c){
 
-        if(sb.equals(word)) return true;
+        if(r<0 || c<0 || r>=board.length || c>=board[0].length)return false;
+        if(idx == word.length()) return true;
+        if(board[r][c] != word.charAt(idx)) return false;
         if(visited[r][c]) return false;
 
-        char ch = board[r][c];
         visited[r][c] = true;        
         
         boolean up = false,down = false,left = false, right = false;
         //up
-        if(r > 0){
-            up = recursion(board, word, sb+ch, visited, r-1, c);
-        }
+        up = recursion(board, word, idx+1, visited, r-1, c);
         //down
-        if(r < board.length-1){
-            down = recursion(board, word, sb+ch, visited, r+1, c);
-        }
+        down = recursion(board, word, idx+1, visited, r+1, c);
         //left
-        if(c > 0){
-            left = recursion(board, word, sb+ch, visited, r, c-1);
-        }
+        left = recursion(board, word, idx+1, visited, r, c-1);
         //right
-        if(c < board[0].length-1){
-            right = recursion(board, word, sb+ch, visited, r, c+1);
-        }
+        right = recursion(board, word, idx+1, visited, r, c+1);
 
         visited[r][c] = false;
 
